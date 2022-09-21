d2c57de13349d0115aa737829342e1f74de08eba
INES ATTIA <wes.ines@gmail.com>
2022-09-21 12:30:53 +0200

    Ionic test Leaflet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -6,11 +7,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserService } from './services/User.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    UserService,
+    HttpClient,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
-
+import { UserService } from '../services/User.service';
+import * as Leaflet from 'leaflet';
+import { icon } from 'leaflet';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  map: Leaflet.Map;
+  propertyList = [];
 
-  constructor() {}
+  constructor(private userservice: UserService) {}
 
+  ionViewDidEnter() {
+    this.map = new Leaflet.Map('mapId').setView([48.633333, 4.365489], 16);
+
+    Leaflet.tileLayer(
+      'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
+    ).addTo(this.map);
+    this.userservice.loadFromAPI().subscribe((data) => {
+      data.forEach((item) => {
+        const obj = { lat: item.lat, lng: item.lng, city: item.city };
+        console.log(obj);
+
+        this.propertyList.push(obj);
+        this.leafletMap();
+      });
+    });
+  }
+  leafletMap() {
+    for (const property of this.propertyList) {
+      console.log(property);
+      Leaflet.marker([property.lat, property.lng], {
+        icon: icon({
+          iconSize: [20, 50], // size of the icon
+          shadowSize: [1, 2], // size of the shadow
+          iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+          shadowAnchor: [4, 62], // the same for the shadow
+          popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+          iconUrl: './../../assets/icon/marker.png',
+          shadowUrl: './../../assets/icon/marker.png',
+        }),
+      })
+        .addTo(this.map)
+        .bindPopup(property.city);
+      // .openPopup();
+    }
+  }
+
+  ionViewWillLeave() {
+    this.map.remove();
+  }
 }
